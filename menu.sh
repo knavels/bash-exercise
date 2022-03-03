559250325c22ed374cd58d63451f8cd20fe3e21c
Navid Dezashibi <dezashibi@gmail.com>
2022-03-03 15:40:14 +0400

    simple menu system using select in var

---
+++ menu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+names='Kyle Cartman Stan Quit'
+
+PS3='Select character: '
+
+select name in $names
+do
+    if [ $name == 'Quit' ]
+    then
+        break
+    fi
+    echo "Hello $name"
+done
+
+echo Bye
+
